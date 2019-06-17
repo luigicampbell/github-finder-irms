0bbc52e0e2ee963b0321e38467af99cf2e369828
Luigi Campbell <luigi.campbell@outlook.com>
2019-06-16 21:21:06 -0700

    changed stateful components to functional components and moved state to top

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import UserCollection from './components/User/UserCollection';
 import './App.css';
 
 export default class App extends Component {
+  state = {
+    users: [],
+    isLoading: true
+  }
+
   constructor() {
     super();
     console.log('CONSTRUCTOR IS CALLED or EXECUTED or INVOKED');
@@ -15,11 +20,12 @@ export default class App extends Component {
 
   render() {
     console.log('RENDER WILL PAINT THE COMPONENT AND CHILDREN');
+    const { isLoading, users } = this.state;
     return (
       <div className="App">
         <Navbar title={"Github Frindr"} iconClass={"fab fa-github"} />
         <div className="container">
-          <UserCollection />
+          <UserCollection isLoading={isLoading} users={users}/>
         </div>
       </div>
     );
@@ -27,5 +33,19 @@ export default class App extends Component {
 
   componentDidMount() {
     console.log('COMPONENT MOUNTED SUCCESSFULLY WITHOUT FATAL ERROR');
+    console.log('MAKE REQUESTS HERE');
+    this.fetchUsers();
+  }
+
+  fetchUsers = async () => {
+    const data = await fetch('https://api.github.com/users');
+    const users = await data.json();
+    this.setState(
+      {
+        users: users,
+        isLoading: false 
+      }
+    );
+    console.log(users);
   }
 }
--- src/components/User/UserCollection.js
+++ src/components/User/UserCollection.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import User from './User';
 
 const userStyle = {
@@ -7,45 +7,22 @@ const userStyle = {
   gridGap: '1rem'
 };
 
-export default class UserCollection extends Component {
-  state = {
-    users: [
+const UserCollection = props => {
+  const { isLoading, users } = props;
+  return (
+    <div style={userStyle}>
       {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        login: 'defunkt',
-        id: 2,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-          login: 'pjhyett',
-          id: 3,
-          avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-          html_url: 'https://github.com/pjhyett',
-      }
-    ]
-  };
-
-  render() {
-    return (
-      <div style={userStyle}>
-        {
-          this.state.users.map(
-            user => (
-              <User 
-                key={user.id} 
-                user={user} 
-               />
-            )
+        users.map(
+          user => (
+            <User 
+              key={user.id} 
+              user={user} 
+              />
           )
-        }
-      </div>
-    );
-  }
+        )
+      }
+    </div>
+  );
 }
 
+export default UserCollection;
